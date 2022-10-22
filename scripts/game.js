a7608cc4babe45262a8cf4ee1a1fcaafa41fd833
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-10-22 16:48:06 -0300

    Declarando globalmente array que armazena as combinações de vitória possíveis

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,16 @@ let board = ['', '', '', '', '', '', '', '', '']
 let playerTime = 0
 let symbols = ['o', 'x']
 let gameOver = false
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 3, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 let handleMove = (position) => {
 
@@ -29,16 +39,6 @@ return gameOver
 }
 
 let isWin = () => {
-    let winStates = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 3, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
 
     for(let i = 0; i < winStates.length; i++) {
         let seq = winStates[i]
